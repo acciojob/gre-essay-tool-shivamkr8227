0672e4d2e5259a1cf77b7dfc3d714c7a347def0d
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-19 22:17:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+function countltr(){
+	const inp=document.getElementById("evaluatedText").value;
+	const ltrcount=inp.length;
+	document.getElementById("wordCount").textContent=ltrcount;
+}
